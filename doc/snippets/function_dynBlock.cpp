0969c1b2b0069d4ca2ccd13add47cbf7a4d21141
Benoit Jacob <jacob.benoit.1@gmail.com>
2007-12-21 09:30:32 +0000

    now cmake takes snippets of code, completes them into compilable sources, builds them, executes them and stores their output in files.

--- doc/snippets/function_dynBlock.cpp
+++ doc/snippets/function_dynBlock.cpp
@@ -1,10 +1,3 @@
-#include <Eigen/Core.h>
-USING_EIGEN_DATA_TYPES
-using namespace std;
-int main(int, char**)
-{
-    Matrix4d m = Matrix4d::identity();
-    m.dynBlock(2,0,2,2) = m.dynBlock(0,0,2,2);
-    cout << m << endl;
-    return 0;
-}
+Matrix4d m = Matrix4d::identity();
+m.dynBlock(2,0,2,2) = m.dynBlock(0,0,2,2);
+cout << m << endl;
