09a37d19c18ff1d9dac00c94da601dcd1f1b97cd
Gael Guennebaud <g.gael@free.fr>
2008-08-22 22:49:09 +0000

    extended cwise unit tests to cover all Cwise API

--- test/cwiseop.cpp
+++ test/cwiseop.cpp
@@ -37,6 +37,7 @@ template<typename Scalar> struct AddIfNull {
 template<typename MatrixType> void cwiseops(const MatrixType& m)
 {
   typedef typename MatrixType::Scalar Scalar;
+  typedef typename NumTraits<Scalar>::Real RealScalar;
   typedef Matrix<Scalar, MatrixType::RowsAtCompileTime, 1> VectorType;
 
   int rows = m.rows();
@@ -54,26 +55,68 @@ template<typename MatrixType> void cwiseops(const MatrixType& m)
              v2 = test_random_matrix<VectorType>(rows),
              vzero = VectorType::Zero(rows);
 
+  int r = ei_random<int>(0, rows-1),
+      c = ei_random<int>(0, cols-1);
+
   m2 = m2.template binaryExpr<AddIfNull<Scalar> >(mones);
 
-  VERIFY_IS_APPROX(               mzero,    m1-m1);
-  VERIFY_IS_APPROX(               m2,       m1+m2-m1);
-#ifdef EIGEN_VECTORIZE
+  VERIFY_IS_APPROX(m1.cwise().pow(2), m1.cwise().abs2());
+  VERIFY_IS_APPROX(m1.cwise().pow(2), m1.cwise().square());
+  VERIFY_IS_APPROX(m1.cwise().pow(3), m1.cwise().cube());
+
+  VERIFY_IS_APPROX(m1 + mones, m1.cwise()+Scalar(1));
+  VERIFY_IS_APPROX(m1 - mones, m1.cwise()-Scalar(1));
+  m3 = m1; m3.cwise() += 1;
+  VERIFY_IS_APPROX(m1 + mones, m3);
+  m3 = m1; m3.cwise() -= 1;
+  VERIFY_IS_APPROX(m1 - mones, m3);
+
+  VERIFY_IS_APPROX(m2, m2.cwise() * mones);
+  VERIFY_IS_APPROX(m1.cwise() * m2,  m2.cwise() * m1);
+  
+  VERIFY_IS_APPROX(mones,    m2.cwise()/m2);
   if(NumTraits<Scalar>::HasFloatingPoint)
-#endif
   {
-    VERIFY_IS_APPROX(               mones,    m2.cwise()/m2);
+    VERIFY_IS_APPROX(m1.cwise() / m2,    m1.cwise() * (m2.cwise().inverse()));
+    m3 = m1.cwise().abs().cwise().sqrt();
+    VERIFY_IS_APPROX(m3.cwise().square(), m1.cwise().abs());
+    VERIFY_IS_APPROX(m1.cwise().square().cwise().sqrt(), m1.cwise().abs());
+    VERIFY_IS_APPROX(m1.cwise().abs().cwise().log().cwise().exp() , m1.cwise().abs());
+    
+//     VERIFY_IS_APPROX(m1.cwise().pow(-1), m1.cwise().inverse());
+//     VERIFY_IS_APPROX(m1.cwise().pow(0.5), m1.cwise().sqrt());
+//     VERIFY_IS_APPROX(m1.cwise().tan(), m1.cwise().sin().cwise() / m1.cwise().cos());
+    VERIFY_IS_APPROX(mones, m1.cwise().sin().cwise().square() + m1.cwise().cos().cwise().square());
   }
-  VERIFY_IS_APPROX(               m1.cwise() * m2,    m2.cwise() * m1);
+
+  // check min
+  VERIFY_IS_APPROX( m1.cwise().min(m2), m2.cwise().min(m1) );
+  VERIFY_IS_APPROX( m1.cwise().min(m1+mones), m1 );
+  VERIFY_IS_APPROX( m1.cwise().min(m1-mones), m1-mones );
+
+  // check max
+  VERIFY_IS_APPROX( m1.cwise().max(m2), m2.cwise().max(m1) );
+  VERIFY_IS_APPROX( m1.cwise().max(m1-mones), m1 );
+  VERIFY_IS_APPROX( m1.cwise().max(m1+mones), m1+mones );
+  
+  VERIFY( (m1.cwise() == m1).all() );
+  VERIFY( (m1.cwise() != m2).any() );
+  VERIFY(!(m1.cwise() == (m1+mones)).any() );
+  if (rows*cols>1)
+  {
+    m3 = m1;
+    m3(r,c) += 1;
+    VERIFY( (m1.cwise() == m3).any() );
+    VERIFY( !(m1.cwise() == m3).all() );
+  }
+  VERIFY( (m1.cwise().min(m2).cwise() <= m2).all() );
+  VERIFY( (m1.cwise().max(m2).cwise() >= m2).all() );
+  VERIFY( (m1.cwise().min(m2).cwise() < (m1+mones)).all() );
+  VERIFY( (m1.cwise().max(m2).cwise() > (m1-mones)).all() );
 
   VERIFY( (m1.cwise()<m1.unaryExpr(bind2nd(plus<Scalar>(), Scalar(1)))).all() );
   VERIFY( !(m1.cwise()<m1.unaryExpr(bind2nd(minus<Scalar>(), Scalar(1)))).all() );
   VERIFY( !(m1.cwise()>m1.unaryExpr(bind2nd(plus<Scalar>(), Scalar(1)))).any() );
-  //VERIFY_IS_APPROX(               m1,       m2.cwiseProduct(m1).cwiseQuotient(m2));
-
-//   VERIFY_IS_APPROX( cwiseMin(m1,m2), cwiseMin(m2,m1) );
-//   VERIFY_IS_APPROX( cwiseMin(m1,m1+mones), m1 );
-//   VERIFY_IS_APPROX( cwiseMin(m1,m1-mones), m1-mones );
 }
 
 void test_cwiseop()
@@ -82,6 +125,7 @@ void test_cwiseop()
     CALL_SUBTEST( cwiseops(Matrix<float, 1, 1>()) );
     CALL_SUBTEST( cwiseops(Matrix4d()) );
     CALL_SUBTEST( cwiseops(MatrixXf(3, 3)) );
+    CALL_SUBTEST( cwiseops(MatrixXf(22, 22)) );
     CALL_SUBTEST( cwiseops(MatrixXi(8, 12)) );
     CALL_SUBTEST( cwiseops(MatrixXd(20, 20)) );
   }
