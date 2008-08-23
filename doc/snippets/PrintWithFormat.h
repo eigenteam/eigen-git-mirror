8507b869e842e4f83ed994c5ad178c35552286f8
Gael Guennebaud <g.gael@free.fr>
2008-08-23 19:32:13 +0000

    need to commit this file before changing its name

---
+++ doc/snippets/PrintWithFormat.h
@@ -0,0 +1,14 @@
+std::string sep = "\n----------------------------------------\n";
+Matrix3f m1;
+m1 << 0, 1.111111, 2, 3.33333, 4, 5, 6, 7, 8.888888, 9;
+
+IOFormat CommaInitFmt(4, Raw, ", ", ", ", "", "", " << ", ";");
+IOFormat CleanFmt(4, AlignCols, ", ", "\n", "[", "]");
+IOFormat OctaveFmt(4, AlignCols, ", ", ";\n", "", "", "[", "]");
+IOFormat HeavyFmt(4, AlignCols, ", ", ";\n", "[", "]", "[", "]");
+
+std::cout << m1 << sep;
+std::cout << m1.format(CommaInitFmt) << sep;
+std::cout << m1.format(CleanFmt) << sep;
+std::cout << m1.format(OctaveFmt) << sep;
+std::cout << m1.format(HeavyFmt) << sep;
