1f14366f762b7811ea3fb9222991ef8decf857e9
Gael Guennebaud <g.gael@free.fr>
2008-09-13 10:44:23 +0000

    fix doc compilation

--- doc/snippets/MatrixBase_template_int.cpp
+++ doc/snippets/MatrixBase_template_int.cpp
@@ -1,5 +1,5 @@
-RowVector5i v = RowVector5i::Random();
+RowVector4i v = RowVector4i::Random();
 cout << "Here is the vector v:" << endl << v << endl;
-cout << "Here is v.block<2>(1):" << endl << v.start<2>() << endl;
+cout << "Here is v.block<2>(1):" << endl << v.block<2>(1) << endl;
 v.block<2>(2).setZero();
 cout << "Now the vector v is:" << endl << v << endl;
