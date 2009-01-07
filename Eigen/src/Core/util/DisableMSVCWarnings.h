4f26276da881c8a8124708d07b12d07ad49fbdee
Kenneth Frank Riddile <kfriddile@yahoo.com>
2009-01-07 16:50:35 +0000

    * disabled deprecation warning under msvc that was being triggered due to microsoft deprecating portable functions in favor of non-portable ones ( ex: hypot() deprecated in favor of _hypot() )

--- Eigen/src/Core/util/DisableMSVCWarnings.h
+++ Eigen/src/Core/util/DisableMSVCWarnings.h
@@ -3,7 +3,7 @@
 
 #ifdef _MSC_VER
   #pragma warning( push )
-  #pragma warning( disable : 4181 4244 4127 4211 )
+  #pragma warning( disable : 4181 4244 4127 4211 4996 )
 #endif
 
 #endif // EIGEN_DISABLEMSVCWARNINGS_H
