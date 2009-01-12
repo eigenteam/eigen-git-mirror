0c127385a67ea2c1141dc6053868ef116e9a5313
Gael Guennebaud <g.gael@free.fr>
2009-01-12 14:46:11 +0000

    disable/enable msvc headers are allowed to be included multiple times

--- Eigen/src/Core/util/DisableMSVCWarnings.h
+++ Eigen/src/Core/util/DisableMSVCWarnings.h
@@ -1,9 +1,5 @@
-#ifndef EIGEN_DISABLEMSVCWARNINGS_H
-#define EIGEN_DISABLEMSVCWARNINGS_H
 
 #ifdef _MSC_VER
   #pragma warning( push )
   #pragma warning( disable : 4181 4244 4127 4211 )
 #endif
-
-#endif // EIGEN_DISABLEMSVCWARNINGS_H
--- Eigen/src/Core/util/EnableMSVCWarnings.h
+++ Eigen/src/Core/util/EnableMSVCWarnings.h
@@ -1,9 +1,4 @@
-#ifndef EIGEN_ENABLEMSVCWARNINGS_H
-#define EIGEN_ENABLEMSVCWARNINGS_H
 
 #ifdef _MSC_VER
   #pragma warning( pop )
 #endif
-
-#endif // EIGEN_ENABLEMSVCWARNINGS_H
-
