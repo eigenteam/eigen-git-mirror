10fa8b115b014b10b4f0cd604d1c5db81ba2fd89
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-01-13 16:27:26 +0000

    add EIGEN_FUNCTORS_PLUGIN

--- Eigen/src/Core/Functors.h
+++ Eigen/src/Core/Functors.h
@@ -348,6 +348,12 @@ template<typename Scalar>
 struct ei_functor_traits<ei_scalar_identity_op<Scalar> >
 { enum { Cost = NumTraits<Scalar>::AddCost, PacketAccess = false, IsRepeatable = true }; };
 
+// allow to add new functors and specializations of ei_functor_traits from outside Eigen.
+// this macro is really needed because ei_functor_traits must be specialized after it is declared but before it is used...
+#ifdef EIGEN_FUNCTORS_PLUGIN
+#include EIGEN_FUNCTORS_PLUGIN
+#endif
+
 // all functors allow linear access, except ei_scalar_identity_op. So we fix here a quick meta
 // to indicate whether a functor allows linear access, just always answering 'yes' except for
 // ei_scalar_identity_op.
