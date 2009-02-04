546d4bb94242d1d4473088952ba2d7b9d14762ab
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-02-04 16:57:28 +0000

    oops, #ifdef instead of #if ---> bug

--- test/unalignedassert.cpp
+++ test/unalignedassert.cpp
@@ -92,7 +92,7 @@ void check_unalignedassert_good()
   delete[] y;
 }
 
-#ifdef EIGEN_ARCH_WANTS_ALIGNMENT
+#if EIGEN_ARCH_WANTS_ALIGNMENT
 template<typename T>
 void check_unalignedassert_bad()
 {
@@ -109,7 +109,7 @@ void unalignedassert()
   check_unalignedassert_good<Good1>();
   check_unalignedassert_good<Good2>();
   check_unalignedassert_good<Good3>();
-#ifdef EIGEN_ARCH_WANTS_ALIGNMENT
+#if EIGEN_ARCH_WANTS_ALIGNMENT
   VERIFY_RAISES_ASSERT(check_unalignedassert_bad<Bad4>());
   VERIFY_RAISES_ASSERT(check_unalignedassert_bad<Bad5>());
   VERIFY_RAISES_ASSERT(check_unalignedassert_bad<Bad6>());
@@ -120,7 +120,7 @@ void unalignedassert()
   check_unalignedassert_good<Good9>();
   check_unalignedassert_good<Depends<true> >();
 
-#ifdef EIGEN_ARCH_WANTS_ALIGNMENT
+#if EIGEN_ARCH_WANTS_ALIGNMENT
   VERIFY_RAISES_ASSERT(check_unalignedassert_bad<Depends<false> >());
 #endif
 }
