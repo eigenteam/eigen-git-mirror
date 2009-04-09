1dfd02833ead355f19534eb9373fb8f3421fc55b
Gael Guennebaud <g.gael@free.fr>
2009-04-09 06:03:51 +0000

    relicence Julien Pommier's SSE code to Eigen's licenses

--- Eigen/src/Core/arch/SSE/MathFunctions.h
+++ Eigen/src/Core/arch/SSE/MathFunctions.h
@@ -23,31 +23,9 @@
 // License and a copy of the GNU General Public License along with
 // Eigen. If not, see <http://www.gnu.org/licenses/>.
 
-/* The sin, cos, exp, and log functions of this file come from Julien Pommier's sse
- * math library, which is itself inspired by Intel Approximate Math library,
- * and based on the corresponding algorithms of the cephes math library.
+/* The sin, cos, exp, and log functions of this file come from
+ * Julien Pommier's sse math library: http://gruntthepeon.free.fr/ssemath/
  */
-
-/* Copyright (C) 2007  Julien Pommier
-
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-  1. The origin of this software must not be misrepresented; you must not
-     claim that you wrote the original software. If you use this software
-     in a product, an acknowledgment in the product documentation would be
-     appreciated but is not required.
-  2. Altered source versions must be plainly marked as such, and must not be
-     misrepresented as being the original software.
-  3. This notice may not be removed or altered from any source distribution.
-
-  (this is the zlib license)
-*/
 
 #ifndef EIGEN_MATH_FUNCTIONS_SSE_H
 #define EIGEN_MATH_FUNCTIONS_SSE_H
