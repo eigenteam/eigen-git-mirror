ee182f7966a063fa3d07af7782714e76e837ee81
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-08-14 20:23:13 -0400

    update snippet

--- doc/snippets/IOFormat.cpp
+++ doc/snippets/IOFormat.cpp
@@ -1,11 +1,11 @@
 std::string sep = "\n----------------------------------------\n";
-Matrix3f m1;
+Matrix3d m1;
 m1 << 1.111111, 2, 3.33333, 4, 5, 6, 7, 8.888888, 9;
 
-IOFormat CommaInitFmt(4, Raw, ", ", ", ", "", "", " << ", ";");
-IOFormat CleanFmt(4, AlignCols, ", ", "\n", "[", "]");
-IOFormat OctaveFmt(4, AlignCols, ", ", ";\n", "", "", "[", "]");
-IOFormat HeavyFmt(4, AlignCols, ", ", ";\n", "[", "]", "[", "]");
+IOFormat CommaInitFmt(StreamPrecision, DontAlignCols, ", ", ", ", "", "", " << ", ";");
+IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+IOFormat OctaveFmt(StreamPrecision, 0, ", ", ";\n", "", "", "[", "]");
+IOFormat HeavyFmt(FullPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
 
 std::cout << m1 << sep;
 std::cout << m1.format(CommaInitFmt) << sep;
