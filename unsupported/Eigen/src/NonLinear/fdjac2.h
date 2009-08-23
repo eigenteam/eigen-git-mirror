a7cb50d9e00b7f21673f82213fc6ddc69b04753f
Thomas Capricelli <orzel@freehackers.org>
2009-08-23 05:56:12 +0200

    only indentation

--- unsupported/Eigen/src/NonLinear/fdjac2.h
+++ unsupported/Eigen/src/NonLinear/fdjac2.h
@@ -4,7 +4,7 @@ int ei_fdjac2(
         Matrix< Scalar, Dynamic, 1 >  &x,
         Matrix< Scalar, Dynamic, 1 >  &fvec,
         Matrix< Scalar, Dynamic, Dynamic > &fjac,
-	    Scalar epsfcn,
+        Scalar epsfcn,
         Matrix< Scalar, Dynamic, 1 >  &wa)
 {
     /* Local variables */
@@ -20,28 +20,28 @@ int ei_fdjac2(
 
     eps = ei_sqrt((std::max(epsfcn,epsmch)));
     for (j = 0; j < n; ++j) {
-	temp = x[j];
-	h = eps * ei_abs(temp);
-	if (h == 0.) {
-	    h = eps;
-	}
-	x[j] = temp + h;
-	iflag = Functor::f(x, wa);
-	if (iflag < 0) {
-	    /* goto L30; */
+        temp = x[j];
+        h = eps * ei_abs(temp);
+        if (h == 0.) {
+            h = eps;
+        }
+        x[j] = temp + h;
+        iflag = Functor::f(x, wa);
+        if (iflag < 0) {
+            /* goto L30; */
             return iflag;
-	}
-	x[j] = temp;
-	for (i = 0; i < m; ++i) {
-	    fjac(i,j) = (wa[i] - fvec[i]) / h;
-/* L10: */
-	}
-/* L20: */
+        }
+        x[j] = temp;
+        for (i = 0; i < m; ++i) {
+            fjac(i,j) = (wa[i] - fvec[i]) / h;
+            /* L10: */
+        }
+        /* L20: */
     }
-/* L30: */
+    /* L30: */
     return iflag;
 
-/*     last card of subroutine fdjac2. */
+    /*     last card of subroutine fdjac2. */
 
 } /* fdjac2_ */
 
