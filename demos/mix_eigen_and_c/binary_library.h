255f9d7a46d2f82c4f99c7a2a7b1a4737b746075
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-09-20 06:00:39 -0400

    add legalese

--- demos/mix_eigen_and_c/binary_library.h
+++ demos/mix_eigen_and_c/binary_library.h
@@ -1,3 +1,27 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2009 Benoit Jacob <jacob.benoit.1@gmail.com>
+//
+// Eigen is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// Alternatively, you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// Eigen is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License or the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License and a copy of the GNU General Public License along with
+// Eigen. If not, see <http://www.gnu.org/licenses/>.
+
 // This is a pure C header, no C++ here.
 // The functions declared here will be implemented in C++ but
 // we don't have to know, because thanks to the extern "C" syntax,
