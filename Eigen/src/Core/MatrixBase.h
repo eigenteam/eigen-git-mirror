1d226d6f51270798eed214539c72a5b4580babff
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-10-31 11:49:20 -0400

    this explicit keyword can't hurt

--- Eigen/src/Core/MatrixBase.h
+++ Eigen/src/Core/MatrixBase.h
@@ -159,7 +159,7 @@ template<typename Derived> class MatrixBase
     inline int size() const { return rows() * cols(); }
     /** \returns the number of nonzero coefficients which is in practice the number
       * of stored coefficients. */
-    inline int nonZeros() const { return size(); }
+    inline int nonZeros() const { return derived.nonZeros(); }
     /** \returns true if either the number of rows or the number of columns is equal to 1.
       * In other words, this function returns
       * \code rows()==1 || cols()==1 \endcode
@@ -636,7 +636,7 @@ template<typename Derived> class MatrixBase
     }
 
   private:
-    MatrixBase(int);
+    explicit MatrixBase(int);
     MatrixBase(int,int);
     template<typename OtherDerived> explicit MatrixBase(const MatrixBase<OtherDerived>&);
 };
