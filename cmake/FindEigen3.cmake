40ebf84b4d9174a8c6eaa958efe69f2c5fa9dcb0
Benoit Jacob <jacob.benoit.1@gmail.com>
2009-11-19 12:41:28 -0500

    move signature file to root directory, where it belongs

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -60,7 +60,7 @@ if (EIGEN3_INCLUDE_DIR)
 
 else (EIGEN3_INCLUDE_DIR)
 
-  find_path(EIGEN3_INCLUDE_DIR NAMES Eigen/signature_of_eigen3_matrix_library
+  find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
