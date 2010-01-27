a0e9a8790d1773990261bd214c3ae5c5c09eb7de
Hauke Heibel <hauke.heibel@gmail.com>
2010-01-27 20:34:05 +0100

    Added EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION macro including unit tests and documentation.

--- doc/snippets/DenseBase_LinSpaced.cpp
+++ doc/snippets/DenseBase_LinSpaced.cpp
@@ -1,2 +1,2 @@
-cout << VectorXi::LinSpaced(7,10,4) << endl;
-cout << VectorXd::LinSpaced(0.0,1.0,5) << endl;
+cout << VectorXi::LinSpaced(7,10,4).transpose() << endl;
+cout << VectorXd::LinSpaced(0.0,1.0,5).transpose() << endl;
