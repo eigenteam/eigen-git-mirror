eb65cccaf64739b7ed8c66dfbcb3c348f22f43ce
Benoit Jacob <jacob.benoit.1@gmail.com>
2010-04-16 14:12:53 -0400

    fix ei_blas_traits directaccess check: in the case of vectors, having a nontrivial inner stride is OK.

--- Eigen/src/Core/util/BlasUtil.h
+++ Eigen/src/Core/util/BlasUtil.h
@@ -158,8 +158,8 @@ template<typename XprType> struct ei_blas_traits
     IsComplex = NumTraits<Scalar>::IsComplex,
     IsTransposed = false,
     NeedToConjugate = false,
-    ActualAccess = (    (int(ei_traits<XprType>::Flags)&DirectAccessBit)
-                     && (int(ei_inner_stride_at_compile_time<XprType>::ret) == 1)
+    ActualAccess = (    (int(XprType::Flags)&DirectAccessBit)
+                     && (bool(XprType::IsVectorAtCompileTime) || int(ei_inner_stride_at_compile_time<XprType>::ret) == 1)
                    ) ?  HasDirectAccess : NoDirectAccess
   };
   typedef typename ei_meta_if<int(ActualAccess)==HasDirectAccess,
