e4b36c488ea20afd44456fb7caee96191c052469
Benoit Jacob <jacob.benoit.1@gmail.com>
2010-06-21 21:07:53 -0400

    fix brain dead computation of the aligned bit. When using a max-size that is fixed and not a multiple of 16 bit, we're not aligned.

--- Eigen/src/Core/util/XprHelper.h
+++ Eigen/src/Core/util/XprHelper.h
@@ -96,8 +96,12 @@ class ei_compute_matrix_flags
       row_major_bit = Options&RowMajor ? RowMajorBit : 0,
       inner_max_size = row_major_bit ? MaxCols : MaxRows,
       is_big = inner_max_size == Dynamic,
-      is_packet_size_multiple = (Cols*Rows) % ei_packet_traits<Scalar>::size == 0,
-      aligned_bit = ((Options&AutoAlign) && (is_big || is_packet_size_multiple)) ? AlignedBit : 0,
+      storage_has_fixed_size = MaxRows != Dynamic && MaxCols != Dynamic,
+      storage_has_aligned_fixed_size = storage_has_fixed_size
+                     && ( (MaxCols*MaxRows) % ei_packet_traits<Scalar>::size == 0 ),
+      aligned_bit = (    (Options&AutoAlign)
+                      && (is_big || storage_has_aligned_fixed_size)
+                    ) ? AlignedBit : 0,
       packet_access_bit = ei_packet_traits<Scalar>::size > 1 && aligned_bit ? PacketAccessBit : 0
     };
 
