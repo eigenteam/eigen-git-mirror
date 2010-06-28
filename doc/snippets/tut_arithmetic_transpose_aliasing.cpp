d978f8d0357235b629b9f716be4a6e291fdcfc8f
Gael Guennebaud <g.gael@free.fr>
2010-06-28 13:30:10 +0200

    * update redux section * fix output precision to 3 for the snippets

--- doc/snippets/tut_arithmetic_transpose_aliasing.cpp
+++ doc/snippets/tut_arithmetic_transpose_aliasing.cpp
@@ -1,4 +1,5 @@
 Matrix2i a; a << 1, 2, 3, 4;
 cout << "Here is the matrix a:\n" << a << endl;
-a = a.transpose(); // fails
-cout << "and the aliasing effect:\n" << a << endl;
+
+a = a.transpose(); // !!! do NOT do this !!!
+cout << "and the result of the aliasing effect:\n" << a << endl;
