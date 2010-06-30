482e6073d0977620ee35a47aaf6b7383026ebf6a
Benoit Jacob <jacob.benoit.1@gmail.com>
2010-06-30 19:27:30 -0400

    fix linalg tut; remove the old one

--- doc/examples/TutorialLinAlgSetThreshold.cpp
+++ doc/examples/TutorialLinAlgSetThreshold.cpp
@@ -7,13 +7,10 @@ using namespace Eigen;
 int main()
 {
    Matrix2d A;
-   FullPivLU<Matrix2d> lu;
    A << 2, 1,
         2, 0.9999999999;
-   lu.compute(A);
+   FullPivLU<Matrix2d> lu(A);
    cout << "By default, the rank of A is found to be " << lu.rank() << endl;
-   cout << "Now recomputing the LU decomposition with threshold 1e-5" << endl;
    lu.setThreshold(1e-5);
-   lu.compute(A);
-   cout << "The rank of A is found to be " << lu.rank() << endl;
+   cout << "With threshold 1e-5, the rank of A is found to be " << lu.rank() << endl;
 }
