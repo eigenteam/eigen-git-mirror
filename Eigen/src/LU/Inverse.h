36a61b1aae0f28d22179ebd8250fd84ae5802d85
Thomas Capricelli <orzel@freehackers.org>
2010-07-01 04:27:45 +0200

    shut one more warning

--- Eigen/src/LU/Inverse.h
+++ Eigen/src/LU/Inverse.h
@@ -296,6 +296,7 @@ struct ei_inverse_impl : public ReturnByValue<ei_inverse_impl<MatrixType> >
   template<typename Dest> inline void evalTo(Dest& dst) const
   {
     const int Size = EIGEN_PLAIN_ENUM_MIN(MatrixType::ColsAtCompileTime,Dest::ColsAtCompileTime);
+    EIGEN_ONLY_USED_FOR_DEBUG(Size);
     ei_assert(( (Size<=1) || (Size>4) || (ei_extract_data(m_matrix)!=ei_extract_data(dst)))
               && "Aliasing problem detected in inverse(), you need to do inverse().eval() here.");
 
