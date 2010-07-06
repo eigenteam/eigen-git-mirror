04831b65d277078b6afc8356bd1776d7468dcb7a
Jitse Niesen <jitse@maths.leeds.ac.uk>
2010-07-06 10:48:25 +0100

    Small changes to tutorial page 1.

--- doc/snippets/tut_matrix_assignment_resizing.cpp
+++ doc/snippets/tut_matrix_assignment_resizing.cpp
@@ -1,5 +1,5 @@
 MatrixXf a(2,2);
-cout << "a is of size " << a.rows() << "x" << a.cols() << std::endl;
+std::cout << "a is of size " << a.rows() << "x" << a.cols() << std::endl;
 MatrixXf b(3,3);
 a = b;
-cout << "a is now of size " << a.rows() << "x" << a.cols() << std::endl;
+std::cout << "a is now of size " << a.rows() << "x" << a.cols() << std::endl;
