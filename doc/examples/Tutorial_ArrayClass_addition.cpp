a51ae35c0b8a2569ff8e3b8ca017ae72bbd3ebab
Jitse Niesen <jitse@maths.leeds.ac.uk>
2010-07-12 22:45:57 +0100

    Tutorial page 3: add more cwise operations, condense rest.

--- doc/examples/Tutorial_ArrayClass_addition.cpp
+++ doc/examples/Tutorial_ArrayClass_addition.cpp
@@ -8,15 +8,16 @@ int main()
 {
   ArrayXXf a(3,3);
   ArrayXXf b(3,3);
-  
   a << 1,2,3,
        4,5,6,
        7,8,9;
-
   b << 1,2,3,
        1,2,3,
        1,2,3;
        
-  cout << "a + b = " << endl
-       << a + b << endl;
+  // Adding two arrays
+  cout << "a + b = " << endl << a + b << endl << endl;
+
+  // Subtracting a scalar from an array
+  cout << "a - 2 = " << endl << a - 2 << endl;
 }
--- doc/examples/Tutorial_ArrayClass_mult.cpp
+++ doc/examples/Tutorial_ArrayClass_mult.cpp
@@ -8,13 +8,9 @@ int main()
 {
   ArrayXXf a(2,2);
   ArrayXXf b(2,2);
-  
   a << 1,2,
        3,4;
-
   b << 5,6,
        7,8;
-
-  cout << "a * b = " << endl
-       << a * b << endl;
+  cout << "a * b = " << endl << a * b << endl;
 }
