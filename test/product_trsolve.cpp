bd9582f584f432e5407ffdc27c610570567439dc
Gael Guennebaud <g.gael@free.fr>
2010-07-20 10:32:56 +0200

    it appears only the "on the left" case was tested

--- test/product_trsolve.cpp
+++ test/product_trsolve.cpp
@@ -45,7 +45,7 @@ template<typename Scalar,int Size, int Cols> void trsolve(int size=Size,int cols
 
   enum { order = Size==1 ? RowMajor : ColMajor };
   Matrix<Scalar,Size,Cols,order> cmRhs(size,cols), ref(size,cols);
-  Matrix<Scalar,Size,Cols,order> rmRhs(size,cols);
+  Matrix<Scalar,Size,Cols,RowMajor> rmRhs(size,cols);
 
   cmLhs.setRandom(); cmLhs *= static_cast<RealScalar>(0.1); cmLhs.diagonal().array() += static_cast<RealScalar>(1);
   rmLhs.setRandom(); rmLhs *= static_cast<RealScalar>(0.1); rmLhs.diagonal().array() += static_cast<RealScalar>(1);
