ea8d1f4bbd44c40cc69727fcd3cb92030f08c6de
Gael Guennebaud <g.gael@free.fr>
2010-12-03 11:22:35 +0100

    add main ei_* functions into Eigen2Support

--- test/eigen2support.cpp
+++ test/eigen2support.cpp
@@ -58,6 +58,11 @@ template<typename MatrixType> void eigen2support(const MatrixType& m)
   VERIFY_IS_EQUAL((m1.col(0).template start<1>()), (m1.col(0).segment(0,1)));
   VERIFY_IS_EQUAL((m1.col(0).end(1)), (m1.col(0).segment(rows-1,1)));
   VERIFY_IS_EQUAL((m1.col(0).template end<1>()), (m1.col(0).segment(rows-1,1)));
+  
+  using namespace internal;
+  VERIFY_IS_EQUAL(ei_cos(s1), cos(s1));
+  VERIFY_IS_EQUAL(ei_real(s1), real(s1));
+  VERIFY_IS_EQUAL(ei_abs2(s1), abs2(s1));
 
   m1.minor(0,0);
 }
