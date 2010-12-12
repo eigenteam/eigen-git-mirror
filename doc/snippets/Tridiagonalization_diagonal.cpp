9045c65e00e02a24f31c7c1f43105aea12929b17
Jitse Niesen <jitse@maths.leeds.ac.uk>
2010-12-12 13:53:42 +0000

    Fix compilation of Tridiagonalization_diagonal example. After changeset 63806965d637, matrixT() is a real matrix even if the matrix which is decomposed is complex.

--- doc/snippets/Tridiagonalization_diagonal.cpp
+++ doc/snippets/Tridiagonalization_diagonal.cpp
@@ -3,7 +3,7 @@ MatrixXcd A = X + X.adjoint();
 cout << "Here is a random self-adjoint 4x4 matrix:" << endl << A << endl << endl;
 
 Tridiagonalization<MatrixXcd> triOfA(A);
-MatrixXcd T = triOfA.matrixT();
+MatrixXd T = triOfA.matrixT();
 cout << "The tridiagonal matrix T is:" << endl << triOfA.matrixT() << endl << endl;
 
 cout << "We can also extract the diagonals of T directly ..." << endl;
