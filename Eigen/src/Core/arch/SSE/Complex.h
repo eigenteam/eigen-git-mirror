c26bb3f88382e7eedcd6c0093056c1d7eb65ecd3
Gael Guennebaud <g.gael@free.fr>
2011-02-09 13:04:35 +0100

    workaround gcc 4.2.1 ICE (fix bug 145)

--- Eigen/src/Core/arch/SSE/Complex.h
+++ Eigen/src/Core/arch/SSE/Complex.h
@@ -111,9 +111,17 @@ template<> EIGEN_STRONG_INLINE Packet2cf pset1<Packet2cf>(const std::complex<flo
 
 template<> EIGEN_STRONG_INLINE std::complex<float>  pfirst<Packet2cf>(const Packet2cf& a)
 {
+  #if (defined __GNUC__) && (__GNUC__==4) && (__GNUC_MINOR__==2) && (__GNUC_MINOR__==2) && (__GNUC_PATCHLEVEL__<=3)
+  // workaround gcc 4.2.1 ICE (mac's gcc version) - I'm not sure how the 4.2.2 and 4.2.3 deal with it, but 4.2.4 works well.
+  // this is not performance wise ideal, but who cares...
+  EIGEN_ALIGN16 std::complex<float> res[2];
+  _mm_store_ps((float*)res, a.v);
+  return res[0];
+  #else
   std::complex<float> res;
   _mm_storel_pi((__m64*)&res, a.v);
   return res;
+  #endif
 }
 
 template<> EIGEN_STRONG_INLINE Packet2cf preverse(const Packet2cf& a) { return Packet2cf(_mm_castpd_ps(preverse(_mm_castps_pd(a.v)))); }
