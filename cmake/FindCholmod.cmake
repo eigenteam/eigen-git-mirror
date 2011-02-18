1c2b59719550502ea97180a7cc71d42eafe6815d
Gael Guennebaud <g.gael@free.fr>
2011-02-18 11:23:27 +0100

    it is now up to user of these Find* module to find and link to BLAS and/or LAPACK

--- cmake/FindCholmod.cmake
+++ cmake/FindCholmod.cmake
@@ -1,3 +1,5 @@
+# Cholmod lib usually requires linking to a blas and lapack library.
+# It is up to the user of this module to find a BLAS and link to it.
 
 if (CHOLMOD_INCLUDES AND CHOLMOD_LIBRARIES)
   set(CHOLMOD_FIND_QUIETLY TRUE)
--- cmake/FindSuperLU.cmake
+++ cmake/FindSuperLU.cmake
@@ -1,30 +1,23 @@
 
+# Umfpack lib usually requires linking to a blas library.
+# It is up to the user of this module to find a BLAS and link to it.
+
 if (SUPERLU_INCLUDES AND SUPERLU_LIBRARIES)
   set(SUPERLU_FIND_QUIETLY TRUE)
 endif (SUPERLU_INCLUDES AND SUPERLU_LIBRARIES)
 
-find_package(BLAS)
+find_path(SUPERLU_INCLUDES
+  NAMES
+  supermatrix.h
+  PATHS
+  $ENV{SUPERLUDIR}
+  ${INCLUDE_INSTALL_DIR}
+  PATH_SUFFIXES
+  superlu
+)
 
-if(BLAS_FOUND)
+find_library(SUPERLU_LIBRARIES superlu PATHS $ENV{SUPERLUDIR} ${LIB_INSTALL_DIR})
   
-  find_path(SUPERLU_INCLUDES
-    NAMES
-    supermatrix.h
-    PATHS
-    $ENV{SUPERLUDIR}
-    ${INCLUDE_INSTALL_DIR}
-    PATH_SUFFIXES
-    superlu
-  )
-
-  find_library(SUPERLU_LIBRARIES superlu PATHS $ENV{SUPERLUDIR} ${LIB_INSTALL_DIR})
-  
-  if(SUPERLU_LIBRARIES)
-    set(SUPERLU_LIBRARIES ${SUPERLU_LIBRARIES} ${BLAS_LIBRARIES})
-  endif(SUPERLU_LIBRARIES)
-  
-endif(BLAS_FOUND)
-
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SUPERLU DEFAULT_MSG
                                   SUPERLU_INCLUDES SUPERLU_LIBRARIES)
