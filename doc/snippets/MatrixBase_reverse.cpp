e27b15a016fd7fb9e51f6f50e1cb7d19a0be372e
Thomas Capricelli <orzel@freehackers.org>
2011-04-19 17:41:59 +0200

    be nice with the server : dont use -j3

--- doc/snippets/MatrixBase_reverse.cpp
+++ doc/snippets/MatrixBase_reverse.cpp
@@ -4,5 +4,5 @@ cout << "Here is the reverse of m:" << endl << m.reverse() << endl;
 cout << "Here is the coefficient (1,0) in the reverse of m:" << endl
      << m.reverse()(1,0) << endl;
 cout << "Let us overwrite this coefficient with the value 4." << endl;
-m.reverse()(1,0) = 4;
+//m.reverse()(1,0) = 4;
 cout << "Now the matrix m is:" << endl << m << endl;
