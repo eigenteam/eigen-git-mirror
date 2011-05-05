ffa0dba18ea58deb9e67bde10e8e4c0f05c5dffb
Thomas Capricelli <orzel@freehackers.org>
2011-05-05 18:48:40 +0200

    better fix for gcc 4.6.0 / ptrdiff_t, as suggested by Benoit

--- Eigen/src/Core/arch/AltiVec/Complex.h
+++ Eigen/src/Core/arch/AltiVec/Complex.h
@@ -70,7 +70,7 @@ template<> EIGEN_STRONG_INLINE Packet2cf pset1<Packet2cf>(const std::complex<flo
 {
   Packet2cf res;
   /* On AltiVec we cannot load 64-bit registers, so wa have to take care of alignment */
-  if((std::ptrdiff_t(&from) % 16) == 0)
+  if((ptrdiff_t(&from) % 16) == 0)
     res.v = pload<Packet4f>((const float *)&from);
   else
     res.v = ploadu<Packet4f>((const float *)&from);
