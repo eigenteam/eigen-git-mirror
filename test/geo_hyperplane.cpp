5ad12265a0ccf36a5ff9e6369527c90a865b97a8
Gael Guennebaud <g.gael@free.fr>
2011-05-28 11:38:53 +0200

    finish to fix bug 270: we have to use EIGEN_ALIGN_STATICALLY and not EIGEN_DONT_ALIGN_STATICALLY... (transplanted from 74437c8cd883d60583560561c09fac13c15321c2)

--- test/geo_hyperplane.cpp
+++ test/geo_hyperplane.cpp
@@ -150,7 +150,7 @@ template<typename Scalar> void hyperplane_alignment()
   VERIFY_IS_APPROX(p1->coeffs(), p2->coeffs());
   VERIFY_IS_APPROX(p1->coeffs(), p3->coeffs());
   
-  #if defined(EIGEN_VECTORIZE) && !defined(EIGEN_DONT_ALIGN_STATICALLY)
+  #if defined(EIGEN_VECTORIZE) && EIGEN_ALIGN_STATICALLY
   if(internal::packet_traits<Scalar>::Vectorizable)
     VERIFY_RAISES_ASSERT((::new(reinterpret_cast<void*>(array3u)) Plane3a));
   #endif
