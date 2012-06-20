d451ed3919f0a54d487a0338dd5da2583e889526
Gael Guennebaud <g.gael@free.fr>
2012-06-20 09:54:52 +0200

    fix bug 477: warning with gcc 4.7

--- Eigen/src/Geometry/Transform.h
+++ Eigen/src/Geometry/Transform.h
@@ -39,7 +39,7 @@ struct transform_traits
     Dim = Transform::Dim,
     HDim = Transform::HDim,
     Mode = Transform::Mode,
-    IsProjective = (Mode==Projective)
+    IsProjective = (int(Mode)==int(Projective))
   };
 };
 
