2ff725199a98e2df4bc83f4c72e188f8558d4e9c
Chen-Pang He <jdh8@ms63.hinet.net>
2012-09-05 18:59:32 +0800

    BLAS: implement DSDOT and SDSDOT; update test for them.

--- blas/double.cpp
+++ blas/double.cpp
@@ -2,6 +2,7 @@
 // for linear algebra.
 //
 // Copyright (C) 2009 Gael Guennebaud <gael.guennebaud@inria.fr>
+// Copyright (C) 2012 Chen-Pang He <jdh8@ms63.hinet.net>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
@@ -17,3 +18,21 @@
 #include "level2_impl.h"
 #include "level2_real_impl.h"
 #include "level3_impl.h"
+
+// currently used by DSDOT only
+double* cast_vector_to_double(float* x, int n, int incx)
+{
+  double* ret = new double[n];
+  if(incx<0) vector(ret,n) = vector(x,n,-incx).reverse().cast<double>();
+  else       vector(ret,n) = vector(x,n, incx).cast<double>();
+  return ret;
+}
+
+double BLASFUNC(dsdot)(int* n, float* px, int* incx, float* py, int* incy)
+{
+  if(*n <= 0) return 0;
+  double* x = cast_vector_to_double(px, *n, *incx);
+  double* y = cast_vector_to_double(py, *n, *incy);
+  return vector(x,*n).cwiseProduct(vector(y,*n)).sum();
+}
+
--- blas/single.cpp
+++ blas/single.cpp
@@ -2,6 +2,7 @@
 // for linear algebra.
 //
 // Copyright (C) 2009 Gael Guennebaud <gael.guennebaud@inria.fr>
+// Copyright (C) 2012 Chen-Pang He <jdh8@ms63.hinet.net>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
@@ -17,3 +18,19 @@
 #include "level2_impl.h"
 #include "level2_real_impl.h"
 #include "level3_impl.h"
+
+float BLASFUNC(sdsdot)(int* n, float* alpha, float* px, int* incx, float* py, int* incy)
+{
+  float* x = reinterpret_cast<float*>(px);
+  float* y = reinterpret_cast<float*>(py);
+  float ret = *alpha;
+
+  if(*n>0) {
+    if(*incx==1 && *incy==1)    ret += (vector(x,*n).cwiseProduct(vector(y,*n))).sum();
+    else if(*incx>0 && *incy>0) ret += (vector(x,*n,*incx).cwiseProduct(vector(y,*n,*incy))).sum();
+    else if(*incx<0 && *incy>0) ret += (vector(x,*n,-*incx).reverse().cwiseProduct(vector(y,*n,*incy))).sum();
+    else if(*incx>0 && *incy<0) ret += (vector(x,*n,*incx).cwiseProduct(vector(y,*n,-*incy).reverse())).sum();
+    else if(*incx<0 && *incy<0) ret += (vector(x,*n,-*incx).reverse().cwiseProduct(vector(y,*n,-*incy).reverse())).sum();
+  }
+  return ret;
+}
