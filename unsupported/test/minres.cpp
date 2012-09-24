472af3fff1a2fc6b382ee084d706746e5f15d1c8
giacomo po <gpo@ucla.edu>
2012-09-24 07:47:38 -0700

    moved MINRES to unsupported. Made unit test.

---
+++ unsupported/test/minres.cpp
@@ -0,0 +1,33 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2011 Gael Guennebaud <g.gael@free.fr>
+// Copyright (C) 2012 Giacomo Po <gpo@ucla.edu>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "../../test/sparse_solver.h"
+#include <Eigen/IterativeSolvers>
+
+template<typename T> void test_minres_T()
+{
+  minres<SparseMatrix<T>, DiagonalPreconditioner<T> > minres_colmajor_diag;
+  minres<SparseMatrix<T>, IdentityPreconditioner    > minres_colmajor_I;
+  minres<SparseMatrix<T>, IncompleteLUT<T> >           minres_colmajor_ilut;
+  //minres<SparseMatrix<T>, SSORPreconditioner<T> >     minres_colmajor_ssor;
+
+  CALL_SUBTEST( check_sparse_square_solving(minres_colmajor_diag)  );
+//   CALL_SUBTEST( check_sparse_square_solving(minres_colmajor_I)     );
+  CALL_SUBTEST( check_sparse_square_solving(minres_colmajor_ilut)     );
+  //CALL_SUBTEST( check_sparse_square_solving(minres_colmajor_ssor)     );
+}
+
+void test_minres()
+{
+  for(int i = 0; i < g_repeat; i++) {
+    CALL_SUBTEST_1(test_minres_T<double>());
+    CALL_SUBTEST_2(test_minres_T<std::complex<double> >());
+  }
+}
