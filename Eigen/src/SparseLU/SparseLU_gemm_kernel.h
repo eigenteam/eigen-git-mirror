1528f733906ac5388291e62c0d9ed6e801e18c79
Gael Guennebaud <g.gael@free.fr>
2012-10-30 15:16:47 +0100

    add missing copyright

--- Eigen/src/SparseLU/SparseLU_gemm_kernel.h
+++ Eigen/src/SparseLU/SparseLU_gemm_kernel.h
@@ -1,3 +1,11 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2012 Gael Guennebaud <gael.guennebaud@inria.fr>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #ifndef EIGEN_SPARSELU_GEMM_KERNEL_H
 #define EIGEN_SPARSELU_GEMM_KERNEL_H
