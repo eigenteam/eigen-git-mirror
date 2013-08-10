317e183a750a65fb8284a54734d7ec6f8833e8f6
Hauke Heibel <hauke.heibel@gmail.com>
2013-08-10 19:11:03 +0200

    Added alternative C++11 detection.

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -104,8 +104,9 @@
 
 // Do we support r-value references?
 #if (__has_feature(cxx_rvalue_references) || \
+    (defined(__cplusplus) && __cplusplus >= 201103L) || \
      defined(__GXX_EXPERIMENTAL_CXX0X__) || \
-     (defined(_MSC_VER) && _MSC_VER >= 1600))
+    (defined(_MSC_VER) && _MSC_VER >= 1600))
   #define EIGEN_HAVE_RVALUE_REFERENCES
 #endif
 
