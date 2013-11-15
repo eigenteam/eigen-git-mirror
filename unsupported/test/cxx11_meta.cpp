b7ca600e953ca551eb0e83d855bc30b18f874c44
Christian Seiler <christian@iwakd.de>
2013-11-16 00:03:23 +0100

    C++11/Tensor: Fix copyright headers

--- unsupported/test/cxx11_meta.cpp
+++ unsupported/test/cxx11_meta.cpp
@@ -1,8 +1,7 @@
 // This file is part of Eigen, a lightweight C++ template library
 // for linear algebra.
 //
-// Copyright (C) 2008-2009 Gael Guennebaud <gael.guennebaud@inria.fr>
-// Copyright (C) 2006-2008 Benoit Jacob <jacob.benoit.1@gmail.com>
+// Copyright (C) 2013 Christian Seiler <christian@iwakd.de>
 //
 // This Source Code Form is subject to the terms of the Mozilla
 // Public License v. 2.0. If a copy of the MPL was not distributed
