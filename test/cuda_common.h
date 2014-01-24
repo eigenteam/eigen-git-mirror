1870084e28bd4bf9dcc6c264e2478f4baa6687bd
Gael Guennebaud <g.gael@free.fr>
2014-01-24 12:51:33 +0100

    NVCC: add more debug info

--- test/cuda_common.h
+++ test/cuda_common.h
@@ -86,12 +86,15 @@ void ei_test_init_cuda()
   cudaDeviceProp deviceProp;
   cudaGetDeviceProperties(&deviceProp, device);
   std::cout << "CUDA device info:\n";
+  std::cout << "  name:                        " << deviceProp.name << "\n";
   std::cout << "  capability:                  " << deviceProp.major << "." << deviceProp.minor << "\n";
   std::cout << "  multiProcessorCount:         " << deviceProp.multiProcessorCount << "\n";
   std::cout << "  maxThreadsPerMultiProcessor: " << deviceProp.maxThreadsPerMultiProcessor << "\n";
+  std::cout << "  warpSize:                    " << deviceProp.warpSize << "\n";
   std::cout << "  regsPerBlock:                " << deviceProp.regsPerBlock << "\n";
   std::cout << "  concurrentKernels:           " << deviceProp.concurrentKernels << "\n";
   std::cout << "  clockRate:                   " << deviceProp.clockRate << "\n";
+  std::cout << "  canMapHostMemory:            " << deviceProp.canMapHostMemory << "\n";
   std::cout << "  computeMode:                 " << deviceProp.computeMode << "\n";
 }
 
