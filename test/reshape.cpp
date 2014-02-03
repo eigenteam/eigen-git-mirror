f4c2eb5e4ffb363e2308269757076285e6e4adb6
yoco <peter.xiau@gmail.com>
2014-02-04 02:50:23 +0800

    Improve reshape test case
    - simplify test code
    - add reshape chain

--- test/reshape.cpp
+++ test/reshape.cpp
@@ -12,60 +12,54 @@
 using Eigen::Map;
 using Eigen::MatrixXi;
 
-template <typename MatType>
-void dynamic_reshape_all_size(MatType m, int row, int col) {
-  for(int new_r = 1; new_r <= row * col; ++new_r) {
-    // skip invalid shape
-    if(row * col % new_r != 0)
-      continue;
-
-    // reshape and compare
-    int new_c = row * col / new_r;
-    VERIFY_IS_EQUAL(m.reshape(new_r, new_c),
-                    Map<MatrixXi>(m.data(), new_r, new_c));
-  }
-}
-
 // just test a 4x4 matrix, enumerate all combination manually,
 // so I don't have to do template-meta-programming here.
 template <typename MatType>
-void static_reshape_all_size(MatType m) {
-  VERIFY_IS_EQUAL((m.template reshape< 1, 16>()), Map<MatrixXi>(m.data(),  1, 16));
-  VERIFY_IS_EQUAL((m.template reshape< 2,  8>()), Map<MatrixXi>(m.data(),  2,  8));
-  VERIFY_IS_EQUAL((m.template reshape< 4,  4>()), Map<MatrixXi>(m.data(),  4,  4));
-  VERIFY_IS_EQUAL((m.template reshape< 8,  2>()), Map<MatrixXi>(m.data(),  8,  2));
-  VERIFY_IS_EQUAL((m.template reshape<16,  1>()), Map<MatrixXi>(m.data(), 16,  1));
+void reshape_all_size(MatType m) {
+  typedef Eigen::Map<MatrixXi> MapMat;
+  // dynamic
+  VERIFY_IS_EQUAL((m.template reshape( 1, 16)), MapMat(m.eval().data(),  1, 16));
+  VERIFY_IS_EQUAL((m.template reshape( 2,  8)), MapMat(m.eval().data(),  2,  8));
+  VERIFY_IS_EQUAL((m.template reshape( 4,  4)), MapMat(m.eval().data(),  4,  4));
+  VERIFY_IS_EQUAL((m.template reshape( 8,  2)), MapMat(m.eval().data(),  8,  2));
+  VERIFY_IS_EQUAL((m.template reshape(16,  1)), MapMat(m.eval().data(), 16,  1));
+
+  // static
+  VERIFY_IS_EQUAL((m.template reshape< 1, 16>()), MapMat(m.eval().data(),  1, 16));
+  VERIFY_IS_EQUAL((m.template reshape< 2,  8>()), MapMat(m.eval().data(),  2,  8));
+  VERIFY_IS_EQUAL((m.template reshape< 4,  4>()), MapMat(m.eval().data(),  4,  4));
+  VERIFY_IS_EQUAL((m.template reshape< 8,  2>()), MapMat(m.eval().data(),  8,  2));
+  VERIFY_IS_EQUAL((m.template reshape<16,  1>()), MapMat(m.eval().data(), 16,  1));
+
+  // reshape chain
+  VERIFY_IS_EQUAL(
+    (m
+     .template reshape( 1, 16)
+     .template reshape< 2,  8>()
+     .template reshape(16,  1)
+     .template reshape< 8,  2>()
+     .template reshape( 2,  8)
+     .template reshape< 1, 16>()
+     .template reshape( 4,  4)
+     .template reshape<16,  1>()
+     .template reshape( 8,  2)
+     .template reshape< 4,  4>()
+    ),
+    MapMat(m.eval().data(), 4,  4)
+  );
 }
 
 void test_reshape()
 {
-  // create matrix(row, col) and filled with 0, 1, 2, ...
-  // for all possible shape
-  int row = 4;
-  int col = 4;
-  Eigen::MatrixXi mx(row, col);
-  Eigen::Matrix4i m4(row, col);
-
-  for(int r = 0; r < row; ++r) {
-    for(int c = 0; c < col; ++c) {
-      mx(r, c) = col * c + r;
-      m4(r, c) = col * c + r;
-    }
-  }
-
-  mx.reshape(8, 2).leftCols(2);
+  Eigen::MatrixXi mx = Eigen::MatrixXi::Random(4, 4);
+  Eigen::Matrix4i m4 = Eigen::Matrix4i::Random(4, 4);
 
   // test dynamic-size matrix
-  CALL_SUBTEST(dynamic_reshape_all_size(mx, 4, 4));
-  CALL_SUBTEST(static_reshape_all_size(mx));
+  CALL_SUBTEST(reshape_all_size(mx));
   // test static-size matrix
-  CALL_SUBTEST(dynamic_reshape_all_size(m4, 4, 4));
-  CALL_SUBTEST(static_reshape_all_size(m4));
-
+  CALL_SUBTEST(reshape_all_size(m4));
   // test dynamic-size const matrix
-  CALL_SUBTEST(dynamic_reshape_all_size(static_cast<const Eigen::MatrixXi>(mx), 4, 4));
-  CALL_SUBTEST(static_reshape_all_size(static_cast<const Eigen::MatrixXi>(mx)));
+  CALL_SUBTEST(reshape_all_size(static_cast<const Eigen::MatrixXi>(mx)));
   // test static-size const matrix
-  CALL_SUBTEST(dynamic_reshape_all_size(static_cast<const Eigen::Matrix4i>(m4), 4, 4));
-  CALL_SUBTEST(static_reshape_all_size(static_cast<const Eigen::Matrix4i>(m4)));
+  CALL_SUBTEST(reshape_all_size(static_cast<const Eigen::Matrix4i>(m4)));
 }
