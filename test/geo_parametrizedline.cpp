49a5b76f365a6840bf53fb752fa90f8b380c4eaf
Benoit Steiner <benoit.steiner.goog@gmail.com>
2014-04-04 17:08:47 -0700

    Updated the geo_parametrizedline_2 test for AVX.

--- test/geo_parametrizedline.cpp
+++ test/geo_parametrizedline.cpp
@@ -66,9 +66,9 @@ template<typename Scalar> void parametrizedline_alignment()
   typedef ParametrizedLine<Scalar,4,AutoAlign> Line4a;
   typedef ParametrizedLine<Scalar,4,DontAlign> Line4u;
 
-  EIGEN_ALIGN_DEFAULT Scalar array1[8];
-  EIGEN_ALIGN_DEFAULT Scalar array2[8];
-  EIGEN_ALIGN_DEFAULT Scalar array3[8+1];
+  EIGEN_ALIGN_DEFAULT Scalar array1[16];
+  EIGEN_ALIGN_DEFAULT Scalar array2[16];
+  EIGEN_ALIGN_DEFAULT Scalar array3[16+1];
   Scalar* array3u = array3+1;
 
   Line4a *p1 = ::new(reinterpret_cast<void*>(array1)) Line4a;
