a49509c4967389fc79a082206f3330042394c28b
Chen-Pang He <jdh8@ms63.hinet.net>
2014-07-05 21:50:05 +0800

    Document internal namespace

---
+++ unsupported/Eigen/doxygen.hpp
@@ -0,0 +1,19 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2014 Chen-Pang He <jdh8@ms63.hinet.net>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// As its name suggests, this file is intended to be read by Doxygen.
+// There is nothing defined so we need no #include guard.
+
+//! \brief Namespace containing all symbols from the %Eigen library.
+namespace Eigen {
+
+//! \brief Namespace containing low-level routines from the %Eigen library.
+namespace internal {}
+
+}
