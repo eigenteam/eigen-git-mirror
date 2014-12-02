9902e06141fef565c7320861b2619b00d70d9118
Gael Guennebaud <g.gael@free.fr>
2014-12-02 14:32:55 +0100

    Fix bicgstab example

--- Eigen/src/IterativeLinearSolvers/BiCGSTAB.h
+++ Eigen/src/IterativeLinearSolvers/BiCGSTAB.h
@@ -142,7 +142,7 @@ struct traits<BiCGSTAB<_MatrixType,_Preconditioner> >
   * SparseMatrix<double> A(n,n);
   * // fill A and b
   * BiCGSTAB<SparseMatrix<double> > solver;
-  * solver(A);
+  * solver.compute(A);
   * x = solver.solve(b);
   * std::cout << "#iterations:     " << solver.iterations() << std::endl;
   * std::cout << "estimated error: " << solver.error()      << std::endl;
