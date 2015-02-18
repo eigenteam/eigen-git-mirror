4a0b96b50de0237320c06d25208747179c2e950a
Hauke Heibel <hauke.heibel@gmail.com>
2015-02-18 18:51:08 +0100

    Fixed template parameter.

--- Eigen/src/Core/products/GeneralBlockPanelKernel.h
+++ Eigen/src/Core/products/GeneralBlockPanelKernel.h
@@ -91,11 +91,11 @@ void computeProductBlockingSizes(Index& k, Index& m, Index& n, Index num_threads
     mr = Traits::mr,
     nr = Traits::nr
   };
-  k = std::min<SizeType>(k, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_K);
+  k = std::min<Index>(k, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_K);
   if (k > kr) k -= k % kr;
-  m = std::min<SizeType>(n, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_M);
+  m = std::min<Index>(n, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_M);
   if (m > mr) m -= m % mr;
-  n = std::min<SizeType>(k, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_N);
+  n = std::min<Index>(k, EIGEN_TEST_SPECIFIC_BLOCKING_SIZE_N);
   if (n > nr) n -= n % nr;
   return;
 #endif
