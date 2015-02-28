432b3f045ea3a0c0f27d7bc886279eace7d019c1
Christoph Hertzberg <chtz@informatik.uni-bremen.de>
2015-02-28 18:39:37 +0100

    Replaced POSIX random() by internal::random

--- unsupported/test/cxx11_tensor_comparisons.cpp
+++ unsupported/test/cxx11_tensor_comparisons.cpp
@@ -54,7 +54,7 @@ static void test_equality()
   for (int i = 0; i < 2; ++i) {
     for (int j = 0; j < 3; ++j) {
       for (int k = 0; k < 7; ++k) {
-        if (random() < 0.5) {
+        if (internal::random<bool>()) {
           mat2(i,j,k) = mat1(i,j,k);
         }
       }
