b7688b33595cba7d8d58806284678e4111a8dbc9
Benoit Jacob <benoitjacob@google.com>
2015-03-03 11:43:56 -0500

    Add missing copyright notices

--- bench/analyze-blocking-sizes.cpp
+++ bench/analyze-blocking-sizes.cpp
@@ -1,3 +1,12 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2015 Benoit Jacob <benoitjacob@google.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <iostream>
 #include <cstdint>
 #include <cstdlib>
