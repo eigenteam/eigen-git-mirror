0c1d2fad59082808cc30491ae682e06c963cad57
Benoit Steiner <benoit.steiner.goog@gmail.com>
2015-07-01 12:40:27 -0700

    Fixed a couple of mistakes in the previous commit.

--- unsupported/Eigen/CXX11/src/Tensor/TensorForcedEval.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorForcedEval.h
@@ -116,7 +116,8 @@ struct TensorEvaluator<const TensorForcedEvalOp<ArgType>, Device>
     }
     typedef TensorEvalToOp<const ArgType> EvalTo;
     EvalTo evalToTmp(m_buffer, m_op);
-    internal::TensorExecutor<const EvalTo, Device>::run(evalToTmp, m_device);
+    static const bool PacketAccess = TensorEvaluator<ArgType, Device>::PacketAccess;
+    internal::TensorExecutor<const EvalTo, Device, PacketAccess>::run(evalToTmp, m_device);
     m_impl.cleanup();
     return true;
   }
