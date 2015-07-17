dc7691ee80fbf2a4d83dea6ac0635d53b9329ad3
Benoit Steiner <benoit.steiner.goog@gmail.com>
2015-07-17 15:44:26 -0700

    Fixed a const correctness issue in TensorLayoutSwap

--- unsupported/Eigen/CXX11/src/Tensor/TensorLayoutSwap.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorLayoutSwap.h
@@ -157,7 +157,7 @@ struct TensorEvaluator<const TensorLayoutSwapOp<ArgType>, Device>
     return m_impl.template packet<LoadMode>(index);
   }
 
-  EIGEN_DEVICE_FUNC CoeffReturnType* data() const { return m_impl.data(); }
+  EIGEN_DEVICE_FUNC Scalar* data() const { return m_impl.data(); }
 
   const TensorEvaluator<ArgType, Device>& impl() const { return m_impl; }
 
