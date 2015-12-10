467225b7221ed0597f7787f0614599006b6fa5c1
Benoit Steiner <benoit.steiner.goog@gmail.com>
2015-12-10 15:34:57 -0800

    Preliminary support for AVX512

---
+++ Eigen/src/Core/arch/AVX512/PacketMath.h
@@ -0,0 +1,14 @@
+// This file is part of Eigen, a lightweight C++ template library
+// for linear algebra.
+//
+// Copyright (C) 2014 Benoit Steiner (benoit.steiner.goog@gmail.com)
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef EIGEN_PACKET_MATH_AVX512_H
+#define EIGEN_PACKET_MATH_AVX512_H
+
+
+#endif // EIGEN_PACKET_MATH_AVX512_H
