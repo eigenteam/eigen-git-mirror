da1b39ec402253f90d0239a6dabf1869e8f038fd
Gael Guennebaud <g.gael@free.fr>
2015-12-10 22:24:58 +0100

    Fix storage order of PartialRedux

--- Eigen/src/Core/CoreEvaluators.h
+++ Eigen/src/Core/CoreEvaluators.h
@@ -994,7 +994,7 @@ struct evaluator<PartialReduxExpr<ArgType, MemberOp, Direction> >
     CoeffReadCost = TraversalSize==Dynamic ? HugeCost
                   : TraversalSize * evaluator<ArgType>::CoeffReadCost + int(CostOpType::value),
     
-    Flags = (traits<XprType>::Flags&RowMajorBit) | (evaluator<ArgType>::Flags&HereditaryBits),
+    Flags = (traits<XprType>::Flags&RowMajorBit) | (evaluator<ArgType>::Flags&(HereditaryBits&(~RowMajorBit))),
     
     Alignment = 0 // FIXME this will need to be improved once PartialReduxExpr is vectorized
   };
