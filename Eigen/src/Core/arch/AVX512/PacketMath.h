dd89f4f5609725283f3ad8ccdf8d2af440f67de3
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-01-14 16:36:29 -0800

    Deleted unnecessary commas and semicolons

--- Eigen/src/Core/arch/AVX512/PacketMath.h
+++ Eigen/src/Core/arch/AVX512/PacketMath.h
@@ -78,7 +78,7 @@ template<> struct packet_traits<double> : default_packet_traits
     HasSqrt = 0,
     HasRsqrt = 0,
     HasSelect = 1,
-    HasEq = 1,
+    HasEq = 1
   };
 };
 
@@ -141,12 +141,12 @@ EIGEN_STRONG_INLINE Packet16f plset<float>(const float& a) {
       _mm512_set1_ps(a),
       _mm512_set_ps(15.0f, 14.0f, 13.0f, 12.0f, 11.0f, 10.0f, 9.0f, 8.0f, 7.0f, 6.0f, 5.0f,
                     4.0f, 3.0f, 2.0f, 1.0f, 0.0f));
-};
+}
 template <>
 EIGEN_STRONG_INLINE Packet8d plset<double>(const double& a) {
   return _mm512_add_pd(_mm512_set1_pd(a),
                        _mm512_set_pd(7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0));
-};
+}
 
 template <>
 EIGEN_STRONG_INLINE Packet16f padd<Packet16f>(const Packet16f& a,
