f8b172166804f665edda80b6a293eaf054c66119
Ville Kallioniemi <ville.kallioniemi@gmail.com>
2016-01-26 23:41:01 -0700

    Add constructor for long types.

--- unsupported/Eigen/CXX11/src/Tensor/TensorUInt128.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorUInt128.h
@@ -40,6 +40,12 @@ struct TensorUInt128
   EIGEN_DEVICE_FUNC EIGEN_ALWAYS_INLINE
   TensorUInt128(unsigned int x) : high(0), low(x) { }
   EIGEN_DEVICE_FUNC EIGEN_ALWAYS_INLINE
+  TensorUInt128(long x) : high(0), low(x) {
+    eigen_assert(x >= 0);
+  }
+  EIGEN_DEVICE_FUNC EIGEN_ALWAYS_INLINE
+  TensorUInt128(unsigned long x) : high(0), low(x) { }
+  EIGEN_DEVICE_FUNC EIGEN_ALWAYS_INLINE
   TensorUInt128(int64_t x) : high(0), low(x) {
     eigen_assert(x >= 0);
   }
