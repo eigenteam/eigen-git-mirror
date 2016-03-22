480ed536ddc309fcc302aad92874a85efd14f810
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-03-22 14:27:34 -0700

    Fixed a couple of typos

--- unsupported/Eigen/CXX11/src/Tensor/TensorDeviceThreadPool.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorDeviceThreadPool.h
@@ -27,7 +27,7 @@ class ThreadPoolInterface {
 class ThreadPool : public ThreadPoolInterface {
  public:
   // Construct a pool that contains "num_threads" threads.
-  explicit ThreadPool(int num_threads) : threads_(num_threads), waiting_(num_threads) {
+  explicit ThreadPool(int num_threads) : threads_(num_threads, NULL), waiting_(num_threads, NULL) {
     for (int i = 0; i < num_threads; i++) {
       threads_.push_back(new std::thread([this]() { WorkerLoop(); }));
     }
