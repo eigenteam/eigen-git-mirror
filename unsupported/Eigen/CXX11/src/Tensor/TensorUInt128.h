8fa08fed3f0c1f65f8196d26cd838cdcc0a78a0e
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-03-30 18:37:19 -0700

    Fixed a off-by-one bug in a debug assertion

--- unsupported/Eigen/CXX11/src/Tensor/TensorUInt128.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorUInt128.h
@@ -55,7 +55,7 @@ struct TensorUInt128
   explicit TensorUInt128(const T& x) : high(0), low(x) {
     typedef typename conditional<sizeof(T) == 8, uint64_t, uint32_t>::type UnsignedT;
     typedef typename conditional<sizeof(LOW) == 8, uint64_t, uint32_t>::type UnsignedLow;
-    eigen_assert(static_cast<UnsignedT>(x) < static_cast<UnsignedLow>(NumTraits<LOW>::highest()));
+    eigen_assert(static_cast<UnsignedT>(x) <= static_cast<UnsignedLow>(NumTraits<LOW>::highest()));
     eigen_assert(x >= 0);
   }
 
