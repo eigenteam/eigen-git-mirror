f0fc3a2b2864374c3183ae4f5b3f6b696b9a1c3f
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-05-25 22:16:15 -0700

    Made the static storage class qualifier come first.

--- unsupported/Eigen/CXX11/src/Tensor/TensorConversion.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorConversion.h
@@ -164,14 +164,14 @@ class TensorConversionOp : public TensorBase<TensorConversionOp<TargetType, XprT
 };
 
 template <bool SameType, typename Eval, typename Scalar> struct ConversionSubExprEval {
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE static bool run(Eval& impl, Scalar*) {
+  static EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE bool run(Eval& impl, Scalar*) {
     impl.evalSubExprsIfNeeded(NULL);
     return true;
   }
 };
 
 template <typename Eval, typename Scalar> struct ConversionSubExprEval<true, Eval, Scalar> {
-  EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE static bool run(Eval& impl, Scalar* data) {
+  static EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE bool run(Eval& impl, Scalar* data) {
     return impl.evalSubExprsIfNeeded(data);
   }
 };
