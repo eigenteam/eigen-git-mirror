6cebc58ac391b893cffe9609b04ea3e10dc5101f
Gael Guennebaud <g.gael@free.fr>
2016-07-07 09:16:40 +0200

    Fix warning

--- unsupported/Eigen/CXX11/src/Tensor/TensorReduction.h
+++ unsupported/Eigen/CXX11/src/Tensor/TensorReduction.h
@@ -558,7 +558,7 @@ struct TensorEvaluator<const TensorReductionOp<Op, Dims, ArgType>, Device>
   EIGEN_DEVICE_FUNC EIGEN_STRONG_INLINE PacketReturnType packet(Index index) const
   {
     EIGEN_STATIC_ASSERT((PacketSize > 1), YOU_MADE_A_PROGRAMMING_MISTAKE)
-    eigen_assert(index + PacketSize - 1 < internal::array_prod(dimensions()));
+    eigen_assert(index + PacketSize - 1 < Index(internal::array_prod(dimensions())));
 
     EIGEN_ALIGN_MAX typename internal::remove_const<CoeffReturnType>::type values[PacketSize];
     if (ReducingInnerMostDims) {
