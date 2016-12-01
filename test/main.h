206858f74b636bbecb12c625410e7cb8a4222237
Gael Guennebaud <g.gael@free.fr>
2016-12-01 16:05:42 +0100

    Fix misleading-indentation warnings.

--- test/main.h
+++ test/main.h
@@ -90,7 +90,7 @@ inline void on_temporary_creation(long int size) {
 #define VERIFY_EVALUATION_COUNT(XPR,N) {\
     nb_temporaries = 0; \
     XPR; \
-    if(nb_temporaries!=N) std::cerr << "nb_temporaries == " << nb_temporaries << "\n"; \
+    if(nb_temporaries!=N) { std::cerr << "nb_temporaries == " << nb_temporaries << "\n"; }\
     VERIFY( (#XPR) && nb_temporaries==N ); \
   }
   
