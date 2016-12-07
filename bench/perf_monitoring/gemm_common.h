271c9d7fe8253b89ac91b634681761c6f61f004f
Gael Guennebaud <g.gael@free.fr>
2016-12-07 16:56:08 +0100

    Add missing include and use -O3

--- bench/perf_monitoring/gemm_common.h
+++ bench/perf_monitoring/gemm_common.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include <Eigen/Core>
 #include "../../BenchTimer.h"
 using namespace Eigen;
--- bench/perf_monitoring/gemv_common.h
+++ bench/perf_monitoring/gemv_common.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 #include <functional>
 #include <Eigen/Core>
 #include "../../BenchTimer.h"
