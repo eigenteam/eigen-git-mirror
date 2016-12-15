6b6410b6a72c8640878d2b63ea3c4c692534f264
Benoit Steiner <benoit.steiner.goog@gmail.com>
2016-12-15 06:49:38 -0800

    Reenabled the use of constexpr on OpenCL devices

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -414,7 +414,7 @@
 // Does the compiler fully support const expressions? (as in c++14)
 #ifndef EIGEN_HAS_CONSTEXPR
 
-#if defined(__CUDACC__) || defined(__SYCL_DEVICE_ONLY__)
+#if defined(__CUDACC__)
 // Const expressions are supported provided that c++11 is enabled and we're using either clang or nvcc 7.5 or above
 #if EIGEN_MAX_CPP_VER>=14 && (__cplusplus > 199711L && defined(__CUDACC_VER__) && (EIGEN_COMP_CLANG || __CUDACC_VER__ >= 70500))
   #define EIGEN_HAS_CONSTEXPR 1
