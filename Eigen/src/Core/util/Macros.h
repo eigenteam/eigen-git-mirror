3b15a61312c164729d0c985f2055a4c727ca9a66
Gael Guennebaud <g.gael@free.fr>
2017-02-10 13:41:52 +0100

    Include clang in the list of non strict MSVC (just to be sure)

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -81,7 +81,7 @@
 //  "15"   15      1900
 
 /// \internal EIGEN_COMP_MSVC_STRICT set to 1 if the compiler is really Microsoft Visual C++ and not ,e.g., ICC or clang-cl
-#if EIGEN_COMP_MSVC && !(EIGEN_COMP_ICC) && !(EIGEN_COMP_LLVM)
+#if EIGEN_COMP_MSVC && !(EIGEN_COMP_ICC || EIGEN_COMP_LLVM || EIGEN_COMP_CLANG)
   #define EIGEN_COMP_MSVC_STRICT _MSC_VER
 #else
   #define EIGEN_COMP_MSVC_STRICT 0
