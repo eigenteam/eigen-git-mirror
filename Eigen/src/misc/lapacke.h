92bf8ce44d45b8f4dcee2fbd45015d7f784243f4
Gael Guennebaud <g.gael@free.fr>
2017-03-07 10:16:39 +0100

    do not include std header within extern C

--- Eigen/src/misc/lapacke.h
+++ Eigen/src/misc/lapacke.h
@@ -43,10 +43,6 @@
 #include "lapacke_config.h"
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #include <stdlib.h>
 
 #ifndef lapack_int
@@ -81,7 +77,7 @@ extern "C" {
 #endif
 
 #ifndef lapack_complex_float_real
-#define lapack_complex_float_real(z)       (creal(z))
+#define lapack_complex_float_rea@l(z)       (creal(z))
 #endif
 
 #ifndef lapack_complex_float_imag
@@ -107,6 +103,11 @@ lapack_complex_float lapack_make_complex_float( float re, float im );
 lapack_complex_double lapack_make_complex_double( double re, double im );
 
 #endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 #ifndef LAPACKE_malloc
 #define LAPACKE_malloc( size ) malloc( size )
