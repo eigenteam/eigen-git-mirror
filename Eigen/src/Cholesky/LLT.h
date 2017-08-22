5d933e94cf09a4fe9d2f0fc61aba718445cc4cfb
Gael Guennebaud <g.gael@free.fr>
2017-08-22 12:46:35 +0200

    Bug 1456: add perf recommendation for LLT and storage format

--- Eigen/src/Cholesky/LLT.h
+++ Eigen/src/Cholesky/LLT.h
@@ -41,6 +41,11 @@ template<typename MatrixType, int UpLo> struct LLT_Traits;
   * Example: \include LLT_example.cpp
   * Output: \verbinclude LLT_example.out
   *
+  * \b Performance: for best performance, it is recommended to use a column-major storage format
+  * with the Lower triangular part (the default), or, equivalently, a row-major storage format,
+  * with the Upper triangular part. Otherwise, you might get a 20% slowdown for the full factorization
+  * step, and rank-updates can be up to 3 times slower.
+  *
   * This class supports the \link InplaceDecomposition inplace decomposition \endlink mechanism.
   *
   * Note that during the decomposition, only the lower (or upper, as defined by _UpLo) triangular part of A is considered.
