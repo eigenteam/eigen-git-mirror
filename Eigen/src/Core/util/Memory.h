244460bc05694fb6e32c16394adb471f6db289a9
Gael Guennebaud <g.gael@free.fr>
2017-09-22 09:23:24 +0200

    Bug 1468 (1/2) : add missing std:: to memcpy (grafted from 395fffe9efd813c05f483159e32273e017ade642)

--- Eigen/src/Core/util/Memory.h
+++ Eigen/src/Core/util/Memory.h
@@ -493,7 +493,7 @@ template<typename T> struct smart_copy_helper<T,true> {
     IntPtr size = IntPtr(end)-IntPtr(start);
     if(size==0) return;
     eigen_internal_assert(start!=0 && end!=0 && target!=0);
-    memcpy(target, start, size);
+    std::memcpy(target, start, size);
   }
 };
 
