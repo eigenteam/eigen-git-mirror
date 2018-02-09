6f61b70df4f3882d8ebf33b633ed994a21174839
Gael Guennebaud <g.gael@free.fr>
2018-02-09 16:43:49 +0100

    Fix typo

--- Eigen/src/Geometry/Scaling.h
+++ Eigen/src/Geometry/Scaling.h
@@ -77,10 +77,10 @@ public:
   /** Concatenates a uniform scaling and an affine transformation */
   template<int Dim, int Mode, int Options>
   inline typename
-	internal::uniformscaling_times_affine_returntype <Scalar,Dim,Mode>::type
+	internal::uniformscaling_times_affine_returntype<Scalar,Dim,Mode>::type
 	operator* (const Transform<Scalar, Dim, Mode, Options>& t) const
   {
-    typename internal::uniformscaling_times_affine_returntype <Scalar,Dim,Mode> res = t;
+    typename internal::uniformscaling_times_affine_returntype<Scalar,Dim,Mode>::type res = t;
     res.prescale(factor());
     return res;
   }
