99005626553de342d2c4b5a6176311d25dc42939
Gael Guennebaud <g.gael@free.fr>
2018-07-19 18:47:18 +0200

    Fix determination of EIGEN_HAS_TYPE_TRAITS

--- Eigen/src/Core/util/Macros.h
+++ Eigen/src/Core/util/Macros.h
@@ -542,7 +542,7 @@
 // Does the compiler support type_traits?
 #ifndef EIGEN_HAS_TYPE_TRAITS
 #if EIGEN_MAX_CPP_VER>=11 && (EIGEN_HAS_CXX11 || EIGEN_COMP_MSVC >= 1700) \
-  && (!EIGEN_COMP_GNUC || EIGEN_GNUC_AT_LEAST(5, 1))
+  && ((!EIGEN_COMP_GNUC_STRICT) || EIGEN_GNUC_AT_LEAST(5, 1))
     // ^^ Full support of type traits was added only to GCC 5.1.0.
 #define EIGEN_HAS_TYPE_TRAITS 1
 #define EIGEN_INCLUDE_TYPE_TRAITS
