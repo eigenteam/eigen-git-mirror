411e98ef575dae7b6884977770bc4b893a639e14
Eugene Zhulenev <ezhulenev@google.com>
2018-11-02 14:51:35 -0700

    Fix code format

--- Eigen/src/Core/util/ReshapedHelper.h
+++ Eigen/src/Core/util/ReshapedHelper.h
@@ -39,10 +39,9 @@ inline Index get_runtime_reshape_size(AutoSize_t /*size*/, Index other, Index to
   return total/other;
 }
 
-template <int Flags, int Order>
-struct get_compiletime_reshape_order
-{
- enum { value = Order==AutoOrder ? Flags & RowMajorBit : Order };
+template<int Flags, int Order>
+struct get_compiletime_reshape_order {
+  enum { value = Order == AutoOrder ? Flags & RowMajorBit : Order };
 };
 
 }
