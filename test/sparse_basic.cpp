efc9ae33ed999d318db366bb305ebe852cc706c7
Gael Guennebaud <g.gael@free.fr>
2018-12-08 00:13:37 +0100

    Fix noise in sparse_basic_3 (numerical cancellation) (grafted from 4c27cc5293d8fe540fb0ab26a2557c6ebdcbf5b3)

--- test/sparse_basic.cpp
+++ test/sparse_basic.cpp
@@ -630,7 +630,8 @@ void big_sparse_triplet(Index rows, Index cols, double density) {
   {
     Index r = internal::random<Index>(0,rows-1);
     Index c = internal::random<Index>(0,cols-1);
-    Scalar v = internal::random<Scalar>();
+    // use positive values to prevent numerical cancellation errors in sum
+    Scalar v = numext::abs(internal::random<Scalar>());
     triplets.push_back(TripletType(r,c,v));
     sum += v;
   }
