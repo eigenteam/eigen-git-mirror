a565da67314720abcd0e9dec87f722a166302333
Hans Johnson <hans.j.johnson@gmail.com>
2019-10-27 07:51:56 -0500

    WIP: Temporary dashboard for tracking down eigen issues.
    This un-official temporary dashboard is intended to assist with
    tracking down issues related to build problems identified by
    the InsightSoftwareConsortium use of Eigen.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,15 +1,19 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "Eigen")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "manao.inria.fr")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Eigen")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Eigen_hjmjohnson")
+# -- set(CTEST_DROP_SITE "manao.inria.fr")
+# -- set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Eigen")
 set(CTEST_DROP_SITE_CDASH TRUE)
 #set(CTEST_PROJECT_SUBPROJECTS
 #Official
