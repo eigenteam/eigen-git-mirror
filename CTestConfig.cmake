0a738cc9daa5b1a382f1992e3ad9bea7b43987c0
Gael Guennebaud <g.gael@free.fr>
2019-11-13 13:33:49 +0100

    Move to my.cdash.org

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,8 +8,8 @@ set(CTEST_PROJECT_NAME "Eigen")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "manao.inria.fr")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Eigen")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Eigen")
 set(CTEST_DROP_SITE_CDASH TRUE)
 #set(CTEST_PROJECT_SUBPROJECTS
 #Official
